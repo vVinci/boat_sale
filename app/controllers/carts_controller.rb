fa7bf245262a8d1337eee48e758995869f52a4c9
Vinicius Vinci <vvinci@protonmail.com>
2022-05-12 21:33:08 -0300

    carts controller created

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,32 @@
 class CartsController < ApplicationController
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    @cart = Cart.new(boat_params)
+    if @cart.save
+      redirect_to cart_path(@cart)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @cart.destroy
+    redirect_to boats_path
+  end
+
+  def remove_cart_item
+    boat = Boat.find(params[:id])
+    @cart = find_cart
+    @cart.remove_boat(boat)
+    redirect_to_index("The item was removed")
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:total_price)
+  end
 end
