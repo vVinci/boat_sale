4ad8432fb99ce8217c38b84fdaff0b56038105e5
Cintia Pizzatto <cintiapizzatto@hotmail.com>
2022-05-17 21:04:26 -0300

    c

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Boat.create(name: "Black Pearl", price: 5000)
-Boat.create(name: "Titanic", price: 10_000)
+Boat.create(name: "NHD 340", price: 540_890)
+Boat.create(name: "Shaefer Yachts 620", price: 398_900)
+Boat.create(name: "Coral-27A", price: 555_900)
+Boat.create(name: "FS-290 Concept", price: 580_900)
+Boat.create(name: "Real Powerboats-415", price: 340_730)
+Boat.create(name: "Malibu-M220", price: 512_000)
